cb2e333aeb76691c671e38db185c1a3b4463c3e5
Guillaume Pinot <texitoi@texitoi.eu>
2014-11-26 00:27:02 +0100

    add deriving to objects

--- src/objects.rs
+++ src/objects.rs
@@ -2,7 +2,7 @@ use std::collections::BTreeMap;
 
 pub type Tags = BTreeMap<String, String>;
 
-#[deriving(Show)]
+#[deriving(Show, PartialEq, PartialOrd, Clone)]
 pub struct Node {
     pub id: i64,
     pub lat: f64,
@@ -10,27 +10,27 @@ pub struct Node {
     pub tags: Tags,
 }
 
-#[deriving(Show)]
+#[deriving(Show, PartialEq, Eq, PartialOrd, Ord, Clone, Hash)]
 pub struct Way {
     pub id: i64,
     pub nodes: Vec<i64>,
     pub tags: Tags,
 }
 
-#[deriving(Show)]
+#[deriving(Show, PartialEq, Eq, PartialOrd, Ord, Clone, Hash)]
 pub enum RelationMember {
     Node(i64),
     Way(i64),
     Relation(i64),
 }
 
-#[deriving(Show)]
+#[deriving(Show, PartialEq, Eq, PartialOrd, Ord, Clone, Hash)]
 pub struct Reference {
     pub member: RelationMember,
     pub role: String,
 }
 
-#[deriving(Show)]
+#[deriving(Show, PartialEq, Eq, PartialOrd, Ord, Clone, Hash)]
 pub struct Relation {
     pub id: i64,
     pub refs: Vec<Reference>,
