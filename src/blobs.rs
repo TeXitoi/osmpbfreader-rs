accc2a9f2971d83cd8e84c455d8021fed4147654
xx37180 <xx37180@ly.com>
2025-03-31 14:36:08 +0800

    feat: add iter_<xxx> for ndoe/way/relation

--- src/blobs.rs
+++ src/blobs.rs
@@ -9,39 +9,49 @@
 
 use std::iter;
 
-use crate::blocks;
-use crate::blocks::OsmObjs as OsmBlockObjs;
+use crate::blocks::{self, OsmObjs as OsmBlockObjs, Relations as BlockRelations, Ways as BlockWays, Nodes as BlockNodes};
 use crate::fileformat::Blob;
 use crate::objects::OsmObj;
 use crate::osmformat::PrimitiveBlock;
 
+macro_rules! wrap {
+    ($name: ident, $wrap_type: ident => $inner_type: path) => {
 self_cell::self_cell!(
-    struct OsmBlobObjs {
+    #[allow(missing_docs)]
+    pub struct $name {
         owner: PrimitiveBlock,
 
         #[covariant]
-        dependent: OsmBlockObjs,
+        dependent: $wrap_type,
     }
 );
 
-impl Iterator for OsmBlobObjs {
-    type Item = OsmObj;
+impl Iterator for $name {
+    type Item = $inner_type;
 
     fn next(&mut self) -> Option<Self::Item> {
         self.with_dependent_mut(|_, objs| objs.next())
     }
 }
 
-/// An iterator on `Result<OsmObj>`.
-pub struct OsmObjs(OsmObjsImpl);
-
-enum OsmObjsImpl {
-    OkIter(iter::Map<OsmBlobObjs, fn(OsmObj) -> crate::Result<OsmObj>>),
-    ErrIter(iter::Once<crate::Result<OsmObj>>),
+    };
 }
 
-impl Iterator for OsmObjs {
-    type Item = crate::Result<OsmObj>;
+wrap!(OsmBlobObjs, OsmBlockObjs => OsmObj);
+wrap!(OsmBlobRelations, BlockRelations => super::Relation);
+wrap!(OsmBlobWays, BlockWays => super::Way);
+wrap!(OsmBlobNodes, BlockNodes => super::Node);
+
+/// An iterator on `Result<OsmObj>`.
+pub struct OsmObjs<T: Iterator>(OsmObjsImpl<T>);
+
+enum OsmObjsImpl<T: Iterator> {
+    OkIter(iter::Map<T, fn(<T as Iterator>::Item) -> crate::Result<<T as Iterator>::Item>>),
+    ErrIter(iter::Once<crate::Result<<T as Iterator>::Item>>),
+}
+
+impl<T: Iterator> Iterator for OsmObjs<T> {
+    type Item = crate::Result<<T as Iterator>::Item>;
     fn next(&mut self) -> Option<Self::Item> {
         match self.0 {
             OsmObjsImpl::OkIter(ref mut iter) => iter.next(),
@@ -51,13 +61,33 @@ impl Iterator for OsmObjs {
 }
 
 /// Transforms a `Result<blob>` into a `Iterator<Item = Result<OsmObj>>`.
-pub fn result_blob_into_iter(result: crate::Result<Blob>) -> OsmObjs {
+pub fn result_blob_into_iter(result: crate::Result<Blob>) -> OsmObjs<OsmBlobObjs> {
     match result.and_then(|b| crate::reader::primitive_block_from_blob(&b)) {
-        Ok(block) => OsmObjs(OsmObjsImpl::OkIter(new_rent_osm_objs(block).map(Ok))),
+        Ok(block) => OsmObjs(OsmObjsImpl::OkIter(OsmBlobObjs::new(block, blocks::iter).map(Ok))),
         Err(e) => OsmObjs(OsmObjsImpl::ErrIter(iter::once(Err(e)))),
     }
 }
 
-fn new_rent_osm_objs(block: PrimitiveBlock) -> OsmBlobObjs {
-    OsmBlobObjs::new(block, blocks::iter)
+/// Transforms a `Result<blob>` into a `Iterator<Item = Result<Node>>`.
+pub fn result_blob_into_node_iter(result: crate::Result<Blob>) -> OsmObjs<OsmBlobNodes> {
+    match result.and_then(|b| crate::reader::primitive_block_from_blob(&b)) {
+        Ok(block) => OsmObjs(OsmObjsImpl::OkIter(OsmBlobNodes::new(block, blocks::nodes).map(Ok))),
+        Err(e) => OsmObjs(OsmObjsImpl::ErrIter(iter::once(Err(e)))),
+    }
+}
+
+/// Transforms a `Result<blob>` into a `Iterator<Item = Result<Way>>`.
+pub fn result_blob_into_way_iter(result: crate::Result<Blob>) -> OsmObjs<OsmBlobWays> {
+    match result.and_then(|b| crate::reader::primitive_block_from_blob(&b)) {
+        Ok(block) => OsmObjs(OsmObjsImpl::OkIter(OsmBlobWays::new(block, blocks::ways).map(Ok))),
+        Err(e) => OsmObjs(OsmObjsImpl::ErrIter(iter::once(Err(e)))),
+    }
+}
+
+/// Transforms a `Result<blob>` into a `Iterator<Item = Result<Relation>>`.
+pub fn result_blob_into_relation_iter(result: crate::Result<Blob>) -> OsmObjs<OsmBlobRelations> {
+    match result.and_then(|b| crate::reader::primitive_block_from_blob(&b)) {
+        Ok(block) => OsmObjs(OsmObjsImpl::OkIter(OsmBlobRelations::new(block, blocks::relations).map(Ok))),
+        Err(e) => OsmObjs(OsmObjsImpl::ErrIter(iter::once(Err(e)))),
+    }
 }
